6b56579afca717a559cfd508ecd227d71b6108e8
didiernyambe <nyambebelle@outlook.com>
2022-11-16 14:20:27 +0100

    second commit

--- main.js
+++ main.js
@@ -1,23 +1,48 @@
-//NOW I SHOULD COUNT THE TABLES AND ADD THE NUMBER OF EACH OF THOSE IN THE ATTRIBUTE ID 
-
-
-const gamezone = document.querySelector('.game-zone');
-const gettables = () => {
-    let numTables = 0;
-    while(gamezone.firstChild){
-        numTables++;
-        gamezone.firstChild.setAttribute('id', numTables);
-        console.log(gamezone.firstChild);
-        // console.log(`Type : ${gamezone.nodeType, numTables}`);
-        // gamezone.removeChild(gamezone.firstChild);
-    } 
-    console.log(`${nbreTables} tableaux`);
-}
-
+alert(' ---- HERE STARTS THE GAME;\n ---- TRY TO FIND ONLY RED APPLES')
 
 
 const attribApples = () => {
-
+    for (let index = 1; index <= 10; index++) {
+        const element = document.getElementById(`${index}`);
+        // reach the td which are children of tr, then of tbody; reason of the (chilnodes[0]) twice
+        const appleTable = element.childNodes[0].childNodes[0].childNodes;
+        if(index < 5){
+            greenApple(1, appleTable);
+        } else if (index >= 5 && index < 8){
+            greenApple(2, appleTable);
+        } else if(index >= 8 && index < 10){
+            greenApple(3, appleTable);
+        } else {
+            greenApple(4, appleTable);
+        }
+    }
 }
 
-gettables();
+// event listener on the tables cells
+const tds = document.getElementsByTagName('td');
+for (const td of tds) {
+    td.addEventListener('click', () => {
+        console.log(td.innerHTML);
+        if (td.innerHTML === "🍏") {
+            alert("A green apple, YOU LOSE");
+        }
+    })
+}
+
+
+// fonction pour l'attribution de la pomme verte
+function greenApple(tableIndex, appleTable) {
+    for (let times = 0; times < tableIndex; times++) {
+        console.log(times);
+        let caseAleatoire = Math.floor((Math.random() * 5));
+        if (appleTable[caseAleatoire].innerHTML === "🍏") {
+            times = times--;
+            continue;
+        } else {
+            appleTable[caseAleatoire].innerHTML = "🍏";
+        }
+    }
+}
+
+attribApples()
+// gettables();
---
+++ readme.md
@@ -0,0 +1,10 @@
+- At the beginning of the game, of the cells must hide their content
+- The gamer has to find the cells that contain a red apple
+- If he clicks on a green apple, he/she loses
+
+
+# DISPOSITION OF THE APPLES
+- The four top lines must have only one green apple
+- lines 6 and 7 should have 2 green apples
+- lines 8 and 9, 3 green apples
+- the last line must have four green apples
